347b1b2f8e4879e418e68018a43025f34a76c8b9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-01 16:56:51 -0400

    add states, add func for input change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
+import Input from './Input';
+import TodoList from './TodoList';
+const data = require('../assets/data.json');
 
 export default function App() {
-  return <div className='App'></div>;
+  // state for todos, All / Active / Completed
+  const [state, setState] = useState();
+  // state for input
+  const [input, setInput] = useState();
+  useEffect(() => { setState('All'); setInput(''); }, []);
+
+  const changeInput = (val) => setInput(val); 
+
+  return (
+    <div className=''>
+      <Input input={input} changeInput={changeInput} />
+      <TodoList state={state} data={data} />
+    </div>
+  );
 }
