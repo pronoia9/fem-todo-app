29ab739ff624ed2cfc4e2d05b36f13b0ff1bf690
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-01 18:25:31 -0400

    init banner done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,10 @@
 import './Banner.scss';
 
 export default function Banner() {
-  return <></>;
+  return (
+    <div class='bg-img-container svelte-7ijpyd'>
+      <img class='bg-img svelte-7ijpyd bg-img--active' src={require('../../assets/images/bg-desktop-dark.jpg')} alt='' />
+      <img class='bg-img svelte-7ijpyd' src={require('../../assets/images/bg-desktop-light.jpg')} alt='' />
+    </div>
+  );
 }
