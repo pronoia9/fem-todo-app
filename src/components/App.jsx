f46d4ee8e655b6426c01548e88f735a7ff599e3d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-12 22:50:29 +0300

    add dark theme to body

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,11 @@ export default function App() {
   const [state, setState] = useState();
   // state for input
   const [input, setInput] = useState();
+
+  //! add dark theme for development purposes only (tldr: i hate white bgs)
+  useEffect(() => {
+    document.getElementsByTagName('body')[0].classList.add('dark-theme');
+  }, [])
   
   useEffect(() => {
     setState('All');
@@ -25,10 +30,45 @@ export default function App() {
       <Banner />
       <Theme />
       <main className='main'>
-        <Input input={input} changeInput={changeInput} />
-        <TodoList state={state} data={data} />
+        {/* <Header /> */}
+        <div className='main__header'>
+          <h1 class='heading svelte-7ijpyd'>Todo</h1>
+          <button aria-label='change the theme of the app' class='svelte-21kiri'>
+            <img src={require('../assets/images/icon-sun.svg').default} alt='theme-button' />
+          </button>
+        </div>
+
+        {/* <Input input={input} changeInput={changeInput} /> */}
+        {/* <TodoList state={state} data={data} /> */}
+
+        <form className='todo-list'>
+          {/* INPUT */}
+          <div className='todo-list__input-wrapper'>
+            <input
+              class='todo-list__input svelte-1o2owkk'
+              type='text'
+              placeholder='Create a new todo...'
+              aria-label='add a new item to the todo list'
+            />
+          </div>
+
+          {/* LIST */}
+          <div className='todo-list__container'>
+            <ul></ul>
+          </div>
+        </form>
       </main>
-      <Footer />
+
+      {/* <Footer /> */}
+      <div class='todo-list__footer'>
+        <span class='items-left'>5 items left</span>
+        <div class='todo-list__options-container'>
+          <button class='todo-list__filter-option active-filter'>All</button>
+          <button class='todo-list__filter-option'>Active</button>
+          <button class='todo-list__filter-option'>Completed</button>
+        </div>
+        <button class='clear-completed'>Clear Completed</button>
+      </div>
     </>
   );
 }
