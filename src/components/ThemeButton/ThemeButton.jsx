c882265b3ff08b9906426f3188c02e9157108430
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 17:48:48 +0300

    add theme button

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -1,5 +1,21 @@
 import './ThemeButton.scss';
 
+import { useStateContext } from '../../contexts/ContextProvider';
+
 export default function ThemeButton() {
-  return <></>;
+  const { darkMode, toggleTheme } = useStateContext();
+  const sun = '../../assets/images/icon-sun.svg',
+    moon = '../../assets/images/icon-sun.svg';
+
+  return (
+    <button aria-label='change the theme of the app' onClick={toggleTheme}>
+      <img
+        src={
+          (darkMode ? require('../../assets/images/icon-sun.svg') : require('../../assets/images/icon-moon.svg'))
+            .default
+        }
+        alt='theme-button'
+      />
+    </button>
+  );
 }
