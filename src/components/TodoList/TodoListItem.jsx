f9af8627c0f3e10273b27e85889bb859e529dbe9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 22:46:59 +0300

    rename todostatus func, fix status prop name

--- src/components/TodoList/TodoListItem.jsx
+++ src/components/TodoList/TodoListItem.jsx
@@ -3,28 +3,27 @@ import './TodoListItem.scss';
 import { useStateContext } from '../../contexts/ContextProvider';
 
 export default function TodoListItem({ todo, idx }) {
-  const { changeTodoStatus, removeTodo } = useStateContext();
-  const { id, title, status } = todo;
-  console.log(idx, todo)
-  
+  const { toggleTodoStatus, removeTodo } = useStateContext();
+  const { id, title, completed } = todo;
+  console.log(idx, todo);
+
   return (
-    <li className={`todo${todo.completed ? ' completed' : ''}`}>
+    <li className={`todo${completed ? ' completed' : ''}`}>
       <label className='label'>
         <input className='checkbox sr-only' />
         <span className='fake-checkbox-wrapper'>
-          <span className='fake-checkbox' onClick={() => changeTodoStatus(todo, idx)}>
-            <img className='icon-check' src={require('../../assets/images/icon-check.svg').default} alt='' aria-hidden='true' />
+          <span className='fake-checkbox' onClick={() => toggleTodoStatus(todo, idx)}>
+            <img
+              className='icon-check'
+              src={require('../../assets/images/icon-check.svg').default}
+              alt=''
+              aria-hidden='true'
+            />
           </span>
         </span>
-        <span className='name'>{title}</span>
+        <span className='name'>{`${title} ${completed ? '✅' : ''}`}</span>
       </label>
-      <div
-        className='btn-close'
-        type='button'
-        aria-label='delete item'
-        onClick={(idx) => {
-          // TODO
-        }}>
+      <div className='btn-close' type='button' aria-label='delete item' onClick={() => removeTodo(id, idx)}>
         <img src={require('../../assets/images/icon-cross.svg').default} alt='cross' />
       </div>
     </li>
