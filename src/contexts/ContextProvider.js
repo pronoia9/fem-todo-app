e65edda34a40670b8f6f684935bc3f3b0358b18d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-14 01:36:37 +0300

    add filter state

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -15,6 +15,7 @@ export const ContextProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(true);
   const [todos, setTodos] = useState(defaultTodos);
   const [input, setInput] = useState('');
+  const [filter, setFilter] = useState('');
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
@@ -38,7 +39,16 @@ export const ContextProvider = ({ children }) => {
 
   return (
     <StateContext.Provider
-      value={{ darkMode, toggleTheme, todos, setTodos, toggleTodoStatus, removeTodo, input, setInput, addTodo }}>
+      value={{
+        darkMode, setDarkMode,
+        toggleTheme,
+        todos, setTodos,
+        toggleTodoStatus,
+        removeTodo,
+        addTodo,
+        input, setInput,
+        filter, setFilter,
+      }}>
       {children}
     </StateContext.Provider>
   );
