a410ddc336551a1b8b5d740454d3dc5129ceb4b9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 16:14:35 +0300

    move initial todo to todolistitem component

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -12,20 +12,7 @@ export default function TodoList() {
         <div className='todo-list__container'>
           <ul class='todo-list__list' id='todo-list'>
             {/* <Todo name={todo.name} bind:completed={todo.completed} id={todo.id} on:deleteTodo={deleteTodo}/> */}
-            <li class='todo'>
-              <label class='label'>
-                <input class='checkbox sr-only' />
-                <span class='fake-checkbox-wrapper'>
-                  <span class='fake-checkbox'>
-                    <img class='icon-check' src='/assets/img/icon-check.svg' alt='' aria-hidden='true' />
-                  </span>
-                </span>
-                <span class='name'>Name</span>
-              </label>
-              <button class='btn-close' type='button' aria-label='delete item'>
-                <img src={require('../../assets/images/icon-cross.svg').default} alt='cross' />
-              </button>
-            </li>
+            <TodoListItem />
           </ul>
 
           <TodoFooter />
--- src/components/TodoList/TodoListItem.jsx
+++ src/components/TodoList/TodoListItem.jsx
@@ -1,5 +1,20 @@
 import './TodoListItem.scss';
 
 export default function TodoListItem() {
-  return <></>;
+  return (
+    <li class='todo'>
+      <label class='label'>
+        <input class='checkbox sr-only' />
+        <span class='fake-checkbox-wrapper'>
+          <span class='fake-checkbox'>
+            <img class='icon-check' src='/assets/img/icon-check.svg' alt='' aria-hidden='true' />
+          </span>
+        </span>
+        <span class='name'>Name</span>
+      </label>
+      <button class='btn-close' type='button' aria-label='delete item'>
+        <img src={require('../../assets/images/icon-cross.svg').default} alt='cross' />
+      </button>
+    </li>
+  );
 }
