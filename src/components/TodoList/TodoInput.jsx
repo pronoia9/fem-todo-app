e19d8edf835eae35d992b9f5a1ed2c174a2ab040
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-14 01:24:23 +0300

    add input state + change state

--- src/components/TodoList/TodoInput.jsx
+++ src/components/TodoList/TodoInput.jsx
@@ -1,13 +1,19 @@
 import './TodoInput.scss';
 
+import { useStateContext } from '../../contexts/ContextProvider';
+
 export default function TodoInput() {
+  const { input, setInput } = useStateContext();
+
   return (
     <div className='todo-list__input-wrapper'>
       <input
-        className='todo-list__input svelte-1o2owkk'
+        className='todo-list__input'
         type='text'
         placeholder='Create a new todo...'
         aria-label='add a new item to the todo list'
+        value={input}
+        onChange={(e) => { setInput(e.target.value); }}
       />
     </div>
   );
