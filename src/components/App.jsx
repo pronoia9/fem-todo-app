8001bddfcf03206b8f46c9d78e4bb1bf218f91e6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 16:11:16 +0300

    move initial todo list form to todolist component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 import Banner from './Banner/Banner';
 import Theme from './Theme/Theme';
-import Input from './Input/Input';
 import TodoList from './TodoList/TodoList';
-import Footer from './Footer/Footer';
 const data = require('../assets/data.json');
 
 export default function App() {
@@ -16,8 +14,8 @@ export default function App() {
   //! add dark theme for development purposes only (tldr: i hate white bgs)
   useEffect(() => {
     document.getElementsByTagName('body')[0].classList.add('dark-theme');
-  }, [])
-  
+  }, []);
+
   useEffect(() => {
     setState('All');
     setInput('');
@@ -39,36 +37,8 @@ export default function App() {
         </div>
 
         {/* <Input input={input} changeInput={changeInput} /> */}
-        {/* <TodoList state={state} data={data} /> */}
-
-        <form className='todo-list'>
-          {/* INPUT */}
-          <div className='todo-list__input-wrapper'>
-            <input
-              class='todo-list__input svelte-1o2owkk'
-              type='text'
-              placeholder='Create a new todo...'
-              aria-label='add a new item to the todo list'
-            />
-          </div>
-
-          {/* LIST */}
-          <div className='todo-list__container'>
-            <ul></ul>
-          </div>
-        </form>
+        <TodoList state={state} data={data} />
       </main>
-
-      {/* <Footer /> */}
-      <div class='todo-list__footer'>
-        <span class='items-left'>5 items left</span>
-        <div class='todo-list__options-container'>
-          <button class='todo-list__filter-option active-filter'>All</button>
-          <button class='todo-list__filter-option'>Active</button>
-          <button class='todo-list__filter-option'>Completed</button>
-        </div>
-        <button class='clear-completed'>Clear Completed</button>
-      </div>
     </>
   );
 }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,57 @@
 import './TodoList.scss';
-import TodoListMenu from './TodoListMenu';
+import TodoInput from './TodoInput';
 import TodoListItem from './TodoListItem';
-import Footer from '../Footer/Footer';
+import TodoFooter from './TodoFooter';
 
 export default function TodoList() {
-  return <>
-    <TodoListMenu />
+  return (
+    <>
+      {/* <TodoListMenu />
     <TodoListItem />
-    <Footer />
-  </>;
+    <Footer /> */}
+      <form className='todo-list'>
+        {/* INPUT */}
+        <div className='todo-list__input-wrapper'>
+          <input
+            class='todo-list__input svelte-1o2owkk'
+            type='text'
+            placeholder='Create a new todo...'
+            aria-label='add a new item to the todo list'
+          />
+        </div>
+
+        <div className='todo-list__container'>
+          {/* LIST */}
+          <ul class='todo-list__list' id='todo-list'>
+            {/* <Todo name={todo.name} bind:completed={todo.completed} id={todo.id} on:deleteTodo={deleteTodo}/> */}
+            <li class='todo'>
+              <label class='label'>
+                <input class='checkbox sr-only' />
+                <span class='fake-checkbox-wrapper'>
+                  <span class='fake-checkbox'>
+                    <img class='icon-check' src='/assets/img/icon-check.svg' alt='' aria-hidden='true' />
+                  </span>
+                </span>
+                <span class='name'>Name</span>
+              </label>
+              <button class='btn-close' type='button' aria-label='delete item'>
+                <img src={require('../../assets/images/icon-cross.svg').default} alt='cross' />
+              </button>
+            </li>
+          </ul>
+
+          {/* <Footer /> */}
+          <div class='todo-list__footer'>
+            <span class='items-left'>5 items left</span>
+            <div class='todo-list__options-container'>
+              <button class='todo-list__filter-option active-filter'>All</button>
+              <button class='todo-list__filter-option'>Active</button>
+              <button class='todo-list__filter-option'>Completed</button>
+            </div>
+            <button class='clear-completed'>Clear Completed</button>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
