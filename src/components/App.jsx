edf447ee0ee0e369c237cad74bd109444018f339
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 21:49:01 +0300

    add useeffect for dark theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import Banner from './Banner/Banner';
 import ThemeButton from './ThemeButton/ThemeButton';
 import TodoList from './TodoList/TodoList';
 import './App.scss';
 
+import { useStateContext } from '../contexts/ContextProvider';
+
 export default function App() {
+  const { darkMode } = useStateContext();
+  useEffect(() => {
+    const doc = document.getElementsByTagName('body')[0];
+    darkMode ? doc.classList.add('dark-theme') : doc.classList.remove('dark-theme');
+  }, [darkMode]);
+
   return (
     <>
       <Banner />
