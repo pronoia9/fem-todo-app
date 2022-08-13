bdcdc921c36c1e3d1f9c566675eda9be53bcabee
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 22:22:41 +0300

    add changetodostatus, fix check icon

--- src/components/TodoList/TodoListItem.jsx
+++ src/components/TodoList/TodoListItem.jsx
@@ -1,22 +1,30 @@
 import './TodoListItem.scss';
 
+import { useStateContext } from '../../contexts/ContextProvider';
+
 export default function TodoListItem({ todo, idx }) {
+  const { changeTodoStatus, removeTodo } = useStateContext();
   const { id, title, status } = todo;
+  console.log(idx, todo)
   
   return (
-    <li className='todo'>
+    <li className={`todo${todo.completed ? ' completed' : ''}`}>
       <label className='label'>
         <input className='checkbox sr-only' />
         <span className='fake-checkbox-wrapper'>
-          <span className='fake-checkbox'>
-            <img className='icon-check' src='/assets/img/icon-check.svg' alt='' aria-hidden='true' />
+          <span className='fake-checkbox' onClick={() => changeTodoStatus(todo, idx)}>
+            <img className='icon-check' src={require('../../assets/images/icon-check.svg').default} alt='' aria-hidden='true' />
           </span>
         </span>
         <span className='name'>{title}</span>
       </label>
-      <div className='btn-close' type='button' aria-label='delete item' onClick={(idx) => {
-        // TODO
-      }}>
+      <div
+        className='btn-close'
+        type='button'
+        aria-label='delete item'
+        onClick={(idx) => {
+          // TODO
+        }}>
         <img src={require('../../assets/images/icon-cross.svg').default} alt='cross' />
       </div>
     </li>
