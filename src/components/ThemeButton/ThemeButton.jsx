28812b1dbb83dbe1094313470a30130285b75387
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 17:50:16 +0300

    format

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -2,20 +2,15 @@ import './ThemeButton.scss';
 
 import { useStateContext } from '../../contexts/ContextProvider';
 
+const sun = require('../../assets/images/icon-sun.svg'),
+  moon = require('../../assets/images/icon-moon.svg');
+
 export default function ThemeButton() {
   const { darkMode, toggleTheme } = useStateContext();
-  const sun = '../../assets/images/icon-sun.svg',
-    moon = '../../assets/images/icon-sun.svg';
 
   return (
     <button aria-label='change the theme of the app' onClick={toggleTheme}>
-      <img
-        src={
-          (darkMode ? require('../../assets/images/icon-sun.svg') : require('../../assets/images/icon-moon.svg'))
-            .default
-        }
-        alt='theme-button'
-      />
+      <img src={(darkMode ? sun : moon).default} alt='theme-button' />
     </button>
   );
 }
