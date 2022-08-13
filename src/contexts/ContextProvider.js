1fb5141c7da49aa3b58c3ff1731f6137922d3174
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 21:45:36 +0300

    comment out toggle

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -3,22 +3,22 @@ import React, { createContext, useContext, useState } from 'react';
 const StateContext = createContext();
 
 const defaultTodos = [
-  { id: 5, todo: 'Complete online JavaScript course', completed: true },
-  { id: 4, todo: 'Jog around the park 3x', completed: false },
-  { id: 3, todo: '10 minutes meditation', completed: false },
-  { id: 2, todo: 'Read for 1 hour', completed: false },
-  { id: 1, todo: 'Pick up groceries', completed: false },
-  { id: 0, todo: 'Complete Todo App on Frontend Mentor', completed: false },
+  { id: 5, title: 'Complete online JavaScript course', completed: true },
+  { id: 4, title: 'Jog around the park 3x', completed: false },
+  { id: 3, title: '10 minutes meditation', completed: false },
+  { id: 2, title: 'Read for 1 hour', completed: false },
+  { id: 1, title: 'Pick up groceries', completed: false },
+  { id: 0, title: 'Complete Todo App on Frontend Mentor', completed: false },
 ];
 
 export const ContextProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const [todos, setTodos] = useState(defaultTodos);
   const [input, setInput] = useState('');
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
-    document.getElementsByTagName('body')[0].classList.toggle('dark-theme');
+    // document.getElementsByTagName('body')[0].classList.toggle('dark-theme');
   };
 
   return (
