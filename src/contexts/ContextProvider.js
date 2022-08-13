9cc482405be52aab8e37b72cd9c4e9940c56adcd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 18:15:56 +0300

    remove state

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,28 +1,29 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
-const initialState = {
-  darkMode: false,
-  todos: [],
-  input: '',
-};
+const defaultTodos = [
+  { id: 5, todo: 'Complete online JavaScript course', completed: true },
+  { id: 4, todo: 'Jog around the park 3x', completed: false },
+  { id: 3, todo: '10 minutes meditation', completed: false },
+  { id: 2, todo: 'Read for 1 hour', completed: false },
+  { id: 1, todo: 'Pick up groceries', completed: false },
+  { id: 0, todo: 'Complete Todo App on Frontend Mentor', completed: false },
+];
 
 export const ContextProvider = ({ children }) => {
-  const [state, setState] = useState(initialState);
   const [darkMode, setDarkMode] = useState(false);
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(defaultTodos);
   const [input, setInput] = useState('');
 
   const toggleTheme = () => {
-    // setState((state) => ({ ...state, theme: state.theme === 'dark' ? 'light' : 'dark' }));
     setDarkMode(!darkMode);
     document.getElementsByTagName('body')[0].classList.toggle('dark-theme');
   };
 
   return (
     <StateContext.Provider
-      value={{ state, setState, darkMode, setDarkMode, toggleTheme, todos, setTodos, input, setInput }}>
+      value={{ darkMode, toggleTheme, todos, setTodos, input, setInput }}>
       {children}
     </StateContext.Provider>
   );
