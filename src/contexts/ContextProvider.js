311013785bd797125dc7d979756506ad1e02d181
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-13 17:27:51 +0300

    add darkmode boolean state

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -3,19 +3,28 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const StateContext = createContext();
 
 const initialState = {
-  theme: 'light',
+  darkMode: false,
   todos: [],
-  input: ''
+  input: '',
 };
 
 export const ContextProvider = ({ children }) => {
   const [state, setState] = useState(initialState);
+  const [darkMode, setDarkMode] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
   const toggleTheme = () => {
-    setState((state) => ({ ...state, theme: state.theme === 'dark' ? 'light' : 'dark' }));
+    // setState((state) => ({ ...state, theme: state.theme === 'dark' ? 'light' : 'dark' }));
+    setDarkMode(!darkMode);
   };
 
-  return <StateContext.Provider value={{ state, setState, toggleTheme }}>{children}</StateContext.Provider>;
+  return (
+    <StateContext.Provider
+      value={{ state, setState, darkMode, setDarkMode, toggleTheme, todos, setTodos, input, setInput }}>
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
