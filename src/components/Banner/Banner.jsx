722339fd04cf3cc91983c45008c6232bab7df675
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-14 02:24:38 +0300

    add dynamic banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,17 @@
 import './Banner.scss';
 
+import { useStateContext } from '../../contexts/ContextProvider';
+
 export default function Banner() {
+  const { darkMode } = useStateContext();
+
   return (
     <div className='bg-img-container'>
-      <img className='bg-img  bg-img--active' src={require('../../assets/images/bg-desktop-dark.jpg')} alt='' />
-      <img className='bg-img ' src={require('../../assets/images/bg-desktop-light.jpg')} alt='' />
+      <img
+        className='bg-img bg-img--active'
+        src={require(`../../assets/images/bg-desktop-${darkMode ? 'dark' : 'light'}.jpg`)}
+        alt='banner'
+      />
     </div>
   );
 }
