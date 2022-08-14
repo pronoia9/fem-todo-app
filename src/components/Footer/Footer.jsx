e6b4b754fffe1c969ed81200d829517c5d28c8be
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-14 17:38:04 +0300

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <div class='main__footer'>
+      <p class='note'>Drag and drop to reorder list.</p>
+    </div>
+  );
+};
