83e8bfe5dc92b3e511a007c65f69821d2428be1f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-15 20:11:09 +0300

    remove unused class name

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ export default function Banner() {
   return (
     <div className='bg-img-container'>
       <img
-        className='bg-img bg-img--active'
+        className='bg-img'
         src={require(`../../assets/images/bg-desktop-${darkMode ? 'dark' : 'light'}.jpg`)}
         alt='banner'
       />
