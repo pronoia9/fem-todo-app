8eab3b217a596793ce7895ac299d2f302cba2ca1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-15 23:20:32 +0300

    fix className

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import './Footer.scss';
 
 export default function Footer() {
   return (
-    <div class='main__footer'>
-      <p class='note'>Drag and drop to reorder list.</p>
+    <div className='main__footer'>
+      <p className='note'>Drag and drop to reorder list.</p>
     </div>
   );
 };
