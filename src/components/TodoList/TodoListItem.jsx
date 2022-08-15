f97717aae1b4b2f0d6ee4853a18ec985bf01b322
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-15 22:09:00 +0300

    wrap list item in a div + add init drag vars/funcs

--- src/components/TodoList/TodoListItem.jsx
+++ src/components/TodoList/TodoListItem.jsx
@@ -6,41 +6,50 @@ import { useStateContext } from '../../contexts/ContextProvider';
 export default function TodoListItem({ todo, idx }) {
   const [checkAnimation, setCheckAnimation] = useState(todo.completed);
   const [deleteAnimation, setDeleteAnimation] = useState(false);
-  const { toggleTodoStatus, removeTodo } = useStateContext();
+  // eslint-disable-next-line no-unused-vars
   const { id, title, completed } = todo;
+  const { toggleTodoStatus, removeTodo, dragging, startDragging, resetDragging } = useStateContext();
+
+  const handleDragStart = () => {}
+  
+  const handleDragEnter = () => { }
+  
+  const handleDragEnd = () => { }
 
   return (
-    <li className={`todo${checkAnimation ? ' completed' : ''}${deleteAnimation ? ' animation--fadeOut' : ''}`}>
-      <label className='label'>
-        <input className='checkbox sr-only' />
-        <span className='fake-checkbox-wrapper'>
-          <span
-            className='fake-checkbox'
-            onClick={() => {
-              setCheckAnimation(!checkAnimation);
-              // IF theres a filter, set timeout for a sec to get the check + strikethrough animation before the todo disappears/filtered out
-              setTimeout(() => toggleTodoStatus(todo), 500);
-            }}>
-            <img
-              className='icon-check'
-              src={require('../../assets/images/icon-check.svg').default}
-              alt=''
-              aria-hidden='true'
-            />
+    <div dragging={dragging === title}>
+      <li className={`todo${checkAnimation ? ' completed' : ''}${deleteAnimation ? ' animation--fadeOut' : ''}`}>
+        <label className='label'>
+          <input className='checkbox sr-only' />
+          <span className='fake-checkbox-wrapper'>
+            <span
+              className='fake-checkbox'
+              onClick={() => {
+                setCheckAnimation(!checkAnimation);
+                // IF theres a filter, set timeout for a sec to get the check + strikethrough animation before the todo disappears/filtered out
+                setTimeout(() => toggleTodoStatus(todo), 500);
+              }}>
+              <img
+                className='icon-check'
+                src={require('../../assets/images/icon-check.svg').default}
+                alt=''
+                aria-hidden='true'
+              />
+            </span>
           </span>
-        </span>
-        <span className='name'>{title}</span>
-      </label>
-      <div
-        className='btn-close'
-        type='button'
-        aria-label='delete item'
-        onClick={() => {
-          setDeleteAnimation(true);
-          setTimeout(() => removeTodo(todo), 500);
-        }}>
-        <img src={require('../../assets/images/icon-cross.svg').default} alt='cross' />
-      </div>
-    </li>
+          <span className='name'>{title}</span>
+        </label>
+        <div
+          className='btn-close'
+          type='button'
+          aria-label='delete item'
+          onClick={() => {
+            setDeleteAnimation(true);
+            setTimeout(() => removeTodo(todo), 500);
+          }}>
+          <img src={require('../../assets/images/icon-cross.svg').default} alt='cross' />
+        </div>
+      </li>
+    </div>
   );
 }
