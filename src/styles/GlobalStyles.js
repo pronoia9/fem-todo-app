e0c9db0da715363a482655403ca20840a56f3548
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 23:10:43 +0300

    format + add font

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,7 +2,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-   :root {
+  @font-face {
+    font-family: 'Josefin Sans';
+    src: url('/fonts/JosefinSans-VariableFont_wght.ttf');
+  }
+
+  :root {
     --color-bright-blue: ${({ theme }) => theme.colorBrightBlue};
     --gradient-primary: ${({ theme }) => theme.gradientPrimary};
     --color-bg: ${({ theme }) => theme.colorBg};
@@ -22,7 +27,9 @@ export const GlobalStyles = createGlobalStyle`
     background-color: var(--color-bg);
   }
 
-  *, *::before, *::after {
+  *,
+  *::before,
+  *::after {
     box-sizing: inherit;
     margin: 0;
     padding: 0;
@@ -48,16 +55,16 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   .sr-only {
-      width: 1px;
-      height: 1px;
-      position: absolute;
-      clip: rect(0 0 0 0);
-      clip-path: inset(100%);
-      white-space: nowrap;
-      overflow: hidden;
+    width: 1px;
+    height: 1px;
+    position: absolute;
+    clip: rect(0 0 0 0);
+    clip-path: inset(100%);
+    white-space: nowrap;
+    overflow: hidden;
   }
 
   *:focus {
-      outline: 2px dotted var(--color-text-active);
+    outline: 2px dotted var(--color-text-active);
   }
 `;
