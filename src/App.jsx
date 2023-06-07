37b1a109e9218a79f1cab8b54e4a1832456af154
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 23:20:12 +0300

    add components + loading state

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,34 @@
 import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import { Banner, Footer, Header, Preloader, TodoList } from './components';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { handleColorSchemeChange, getSystemTheme } from './utils/utils';
 
-export default function App () {
+export default function App() {
   const [theme, setTheme] = useState(getSystemTheme);
+  const [loading, setLoading] = useState(true);
 
   // Listen for changes in the system's color scheme
   useEffect(() => {
     const colorSchemeListener = window.matchMedia('(prefers-color-scheme: dark)');
     colorSchemeListener.addEventListener('change', (e) => handleColorSchemeChange(e, setTheme));
-    return () => { colorSchemeListener.removeEventListener('change', handleColorSchemeChange); };
+    return () => {
+      colorSchemeListener.removeEventListener('change', handleColorSchemeChange);
+    };
   }, []);
+
+  // Disable preloader after 5s //! TEMP DISABLED UNTIL IMPLEMENTATION
+  useEffect(() => { setTimeout(() => { setLoading(false); }, 0); }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      {loading && <Preloader />}
+      <Banner />
+      <Header />
+      <TodoList />
+      <Footer />
     </ThemeProvider>
   );
-};
+}
