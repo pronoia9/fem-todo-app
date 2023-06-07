ddc9c7446377f729f25b360f13ad6565839fcdf8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-08 00:30:40 +0300

    finish init todo list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+import { TodoFooter, TodoInput, TodoListItem } from './';
+import { useStateContext } from '../contexts/ContextProvider';
+
 export default function TodoList() {
-  return <div>TodoList</div>;
+  const { todos, filter } = useStateContext();
+  const [filteredTodos, setFilteredTodos] = useState(todos);
+
+  useEffect(() => {
+    if (filter) setFilteredTodos(todos.filter((t) => t.completed === (filter === 'active' ? false : true)));
+    else setFilteredTodos(todos);
+  }, [todos, setFilteredTodos, filter]);
+
+  return (
+    <Form onSubmit={(e) => e.preventDefault()}>
+      <TodoInput />
+      <Container>
+        <List>
+          {filteredTodos.map((todo, idx) => (
+            <TodoListItem key={todo.id} todo={todo} idx={idx} />
+          ))}
+        </List>
+        <TodoFooter filteredTodos={filteredTodos} />
+      </Container>
+    </Form>
+  );
 }
+
+const Form = styled.form`
+  margin-bottom: 12rem;
+
+  @media only screen and (min-width: 600px) {
+    margin-bottom: 6rem;
+  }
+`;
+
+const Container = styled.div`
+  border-radius: 5px;
+  box-shadow: var(--box-shadow);
+  transition: box-shadow 1s;
+`;
+
+const List = styled.ul`
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  overflow: hidden;
+`;
