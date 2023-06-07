1f7327df43b0cc9125e421928ca2bf6296cb86fb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 23:00:47 +0300

    add design theming

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,9 +2,21 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+   :root {
+    --color-bright-blue: ${({ theme }) => theme.colorBrightBlue};
+    --gradient-primary: ${({ theme }) => theme.gradientPrimary};
+    --color-bg: ${({ theme }) => theme.colorBg};
+    --color-surface: ${({ theme }) => theme.colorSurface};
+    --color-border: ${({ theme }) => theme.colorBorder};
+    --color-text-active: ${({ theme }) => theme.colorTextActive};
+    --color-text-completed: ${({ theme }) => theme.colorTextCompleted};
+    --color-text-1: ${({ theme }) => theme.colorText1};
+    --box-shadow: ${({ theme }) => theme.boxShadow};
+  }
+
   body {
-    background-color: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
+    background-color: var(--color-bg);
+    color: var(--color-text-1);
     transition: background-color 0.3s ease, color 0.3s ease;
   }
 `;
