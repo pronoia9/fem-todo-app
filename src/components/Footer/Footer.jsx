9b1a887ceaa1ed2b83572b9eba02cc3ffcf3f779
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 22:32:48 +0300

    delete all

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.scss';
-
-export default function Footer() {
-  return (
-    <div className='main__footer'>
-      <p className='note'>Drag and drop to reorder list.</p>
-    </div>
-  );
-};
