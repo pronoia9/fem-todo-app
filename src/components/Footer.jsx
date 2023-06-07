10f71b60cc1a23d82cb7954281799a81afeaac15
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 23:49:40 +0300

    finish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Text>Drag and drop to reorder list.</Text>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  color: var(--color-text-1);
+  letter-spacing: -0.2px;
+  line-height: 1.75rem;
+  text-align: center;
+  transition: color 1s;
+`;
+
+const Text = styled.p`
+  margin-bottom: 1rem;
+  font-size: 1.75rem;
+`;
