bb75c9cc519291986d1d2bae28acf0f9ce425ded
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 23:36:33 +0300

    finish init banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
 export default function Banner() {
-  return <div>Banner</div>;
+  return (
+    <Container className='bg-img-container'>
+      <Image className='bg-img' alt='banner' />
+    </Container>
+  );
 }
+
+
+const Container = styled.div`
+  position: absolute;
+  /* z-index: -1; */
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
+const Image = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  transition: opacity 1s;
+  background-image: url(${(props) => `/images/bg-desktop-${props.theme.image}`});
+`;
