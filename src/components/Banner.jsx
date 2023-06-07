436d1dec9e1a03087a0fd19a2b13286ba1a02a71
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-07 23:17:11 +0300

    init components

---
+++ src/components/Banner.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export default function Banner() {
+  return <div>Banner</div>;
+}
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Banner from './Banner';
+import Footer from './Footer';
+import Header from './Header';
+import Preloader from './Preloader';
+import ThemeButton from './ThemeButton';
+import TodoFooter from './TodoFooter';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+import TodoListItem from './TodoListItem';
+
+export { Banner, Footer, Header, Preloader, ThemeButton, TodoFooter, TodoInput, TodoList, TodoListItem };
