7dd23e0855b0e67382efe4d5691e805794c36323
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-08 03:50:06 +0300

    update default data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,8 +1,12 @@
 export const defaultTodos = [
-  { id: 1, title: 'Complete online JavaScript course', completed: true },
-  { id: 2, title: 'Jog around the park 3x', completed: false },
-  { id: 3, title: '10 minutes meditation', completed: false },
-  { id: 4, title: 'Read for 1 hour', completed: false },
-  { id: 5, title: 'Pick up groceries', completed: false },
-  { id: 6, title: 'Complete Todo App on Frontend Mentor', completed: false },
+  { id: 1, title: 'Conquer the world, one task at a time!', completed: false },
+  { id: 2, title: 'Find the missing sock from every laundry load ever', completed: false },
+  { id: 3, title: 'Invent a teleportation device and accidentally teleport to the wrong location', completed: false },
+  { id: 4, title: 'Teach a goldfish how to play fetch', completed: false },
+  { id: 5, title: 'Write a love letter to a pizza', completed: false },
+  { id: 6, title: 'Perfect the art of procrastination (eventually)', completed: true },
+  { id: 7, title: 'Discover the meaning of life, but forget to write it down', completed: false },
+  { id: 8, title: 'Organize a squirrel army for a top-secret mission', completed: false },
+  { id: 9, title: 'Create a time machine and travel back to convince yourself not to eat that expired yogurt', completed: false },
+  { id: 10, title: 'Master the art of parallel universe navigation', completed: false },
 ];
